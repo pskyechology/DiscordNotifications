2f341ca4a615b9defbb8b491b8788dabf17f33e6
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-07-10 01:17:17 +0200

    Add support for canary webhook URLs (#104)

--- includes/DiscordNotifier.php
+++ includes/DiscordNotifier.php
@@ -262,7 +262,7 @@ class DiscordNotifier {
 			&& !isset( $urlParts['port'] )
 			&& !isset( $urlParts['query'] )
 			&& !isset( $urlParts['fragment'] )
-			&& preg_match( "/^(discord|discordapp)\.com$/", $urlParts['host'] )
+			&& preg_match( "/^(?:canary\.)?(discord|discordapp)\.com$/", $urlParts['host'] )
 			&& preg_match( "#^/api/webhooks/[0-9]+/[a-zA-Z0-9_-]*$#", $urlParts['path'] );
 
 		if ( !$isValid ) {
